97c2800c89c9bb392ca23b61ee6e8b105de53ee9
therealnv6 <devrawr@protonmail.com>
2023-06-02 21:35:27 +0200

    update dataset.cpp

--- src/dataset.cpp
+++ src/dataset.cpp
@@ -4,25 +4,25 @@
 namespace data
 {
 	template<>
-	std::string dataset::get<std::string>(std::string key)
+	std::string dataset::get<std::string>(const data::key &key)
 	{
 		return data[key];
 	}
 
 	template<>
-	int dataset::get<int>(std::string key)
+	int dataset::get<int>(const data::key &key)
 	{
 		return std::stoi(data[key]);
 	}
 
 	template<>
-	uint8_t dataset::get<uint8_t>(std::string key)
+	uint8_t dataset::get<uint8_t>(const data::key &key)
 	{
 		return std::stoi(data[key]);
 	}
 
 	template<>
-	uint16_t dataset::get<uint16_t>(std::string key)
+	uint16_t dataset::get<uint16_t>(const data::key &key)
 	{
 		return std::stoi(data[key]);
 	}
